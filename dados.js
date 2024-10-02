63ee97128a47a12e764b983c0c18153743444898
LarySampaio <lahryssah2017@gmail.com>
2024-10-02 11:32:04 -0300

    feat: Novas páginas, melhorias na estilização

--- dados.js
+++ dados.js
@@ -2,22 +2,22 @@ let dados = [
   {
     titulo: "HTML",
     descricao:
-      "HTML (HyperText Markup Language) é a linguagem básica usada para criar e estruturar páginas na web. Ela permite que você organize textos, imagens, links e outros conteúdos em uma página, dizendo ao navegador como exibir cada parte. Pense no HTML como a estrutura de uma casa, onde você define onde ficam as paredes, portas e janelas, para que tudo apareça corretamente quando alguém entra na página.",
-    link: "um.HTML",
+      "HTML é como os tijolos e a madeira de uma casa: ele define a estrutura básica de uma página da web. Imagine que você está construindo um quebra-cabeça: as peças do quebra-cabeça são os elementos HTML, como títulos, parágrafos e imagens. Com HTML, você cria os elementos e estrutura o conteúdo de forma lógica, facilitando a leitura e a compreensão para os visitantes. Em resumo, HTML é a linguagem que define a estrutura e o conteúdo de uma página web. É a base sobre a qual você irá construir o seu site.",
+    link: "html.html",
     tags: "html linguagem de marcação ",
   },
   {
     titulo: "CSS",
     descricao:
-      "CSS (Cascading Style Sheets) é a linguagem usada para estilizar páginas web. Ele define a aparência visual dos elementos HTML como cores, fontes, layout e posicionamento.",
-    link: "um.HTML",
+      " Uma explicação simples CSS é como um maquiador para páginas da web. Imagine que você está criando um cartaz: o CSS é como a tinta, as fontes e os desenhos que você usa para deixar o cartaz bonito e chamativo. Com CSS, você pode escolher as cores, alterar as fontes, criar layouts e adicionar efeitos, personalizando a aparência de uma página web e deixando-a mais atraente e profissional. É como se você estivesse montando um quebra-cabeça: as peças do quebra-cabeça são os elementos da sua página (títulos, parágrafos, imagens, etc.) e o CSS é a cola colorida que você usa para juntar as peças e criar um quadro bonito..",
+    link: "css.html",
     tags: "css estilização",
   },
   {
     titulo: "JavaScript",
     descricao:
-      "JavaScript é uma linguagem de programação que adiciona interatividade às páginas web. Com ele, você pode criar animações, manipular o conteúdo da página e muito mais.",
-    link: "um.HTML",
+      " O JavaScript é a linguagem de programação que torna as páginas da web mais divertidas e úteis, permitindo criar desde simples animações até complexos jogos online. É como dar vida a um livro estático, transformando-o em uma história interativa. Com o JavaScript, você pode criar sites que respondem aos seus comandos. Por exemplo, quando você clica em um botão, o JavaScript pode fazer com que uma nova seção da página apareça, ou até mesmo enviar informações para um servidor. É como ter um assistente pessoal dentro do seu navegador, realizando tarefas e personalizando a sua experiência na web.",
+    link: "js.html",
     tags: "java",
   },
 ];
